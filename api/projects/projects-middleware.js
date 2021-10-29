f1caf5e8d7a4ea8c6ebd8c9860a0b7e004196e33
rdperry031 <rdperry031@gmail.com>
2021-10-29 11:50:53 -0500

    GET request tests for /:id endpoint now passing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,21 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+const Actions = require('./projects-model');
+
+async function validateProjectId(req, res, next){
+    try{
+        const project = await Projects.get(req.params.id)
+        if(!project){
+            res.status(404).json({ message: `Project with id ${req.params.id} does not exist`})
+        }else{
+            req.project = project
+            next()
+        }
+    }catch(error){
+        res.status(500).json({ message: 'problem finding project'})
+    }
+}
+
+module.exports = {
+    validateProjectId,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const Projects = require('./projects-model');
 const Actions = require('../actions/actions-model');
+const { validateProjectId } = require('./projects-middleware')
 
 router.get('/', async (req, res, next) => {
 try{
@@ -14,4 +15,13 @@ try{
 }
 })
 
+router.get('/:id', validateProjectId, async (req, res, next) => {
+    try{
+        res.status(200).json(req.project)
+    }catch(err){
+        next(err)
+    }
+})
+
 module.exports = router
+ 
